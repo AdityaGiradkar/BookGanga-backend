793f0fe1f166a5f634804a561783e7b8e22f0381
AdityaGiradkar <51029130+AdityaGiradkar@users.noreply.github.com>
2021-04-06 17:15:26 +0530

    database connection updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose')
 
-
 //database user_name = BookGanga
 //database password = BookGanga123
 //database name = BookGangaDB
 
-
-// const DbUrl = 'mongodb+srv://BookGanga:BookGanga123@bookganga.9s1u5.mongodb.net/BookGanga?retryWrites=true&w=majority';
 const DbUrl = 'mongodb+srv://BookGanga:BookGanga123@bookgangadb.anlus.mongodb.net/BookGangaDB?retryWrites=true&w=majority';
-// mongoose.connect(DbUrl, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// });
 
 mongoose.connect(DbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
+
+var con = mongoose.connection;
+con.on('connected', function() {
+    console.log('database is connected successfully');
+});
+
+con.on('disconnected', function() {
+    console.log('database is disconnected successfully');
+})
+
+con.on('error', console.error.bind(console, 'connection error:'));
+
+module.exports = con;
